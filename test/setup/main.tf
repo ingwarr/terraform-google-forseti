cefd6d555a6c0c63e7726355036488418c17ac4a
Ihor Pukha <sorceress1979@gmail.com>
2019-09-18 12:23:56 +0300

    environment is ready

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -11,11 +11,29 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.``
+ * limitations under the License.
  */
 
 
-// Define a host project for the Forseti shared VPC test suite.
+module "forseti-enforcer-project" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 3.0"
+
+  name              = "ci-forseti-enforcer"
+  random_project_id = true
+  org_id            = var.org_id
+  folder_id         = var.folder_id
+  billing_account   = var.billing_account
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "pubsub.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+  ]
+}
 
 module "forseti-host-project" {
   source  = "terraform-google-modules/project-factory/google"
@@ -35,8 +53,36 @@ module "forseti-host-project" {
     "storage-api.googleapis.com",
     "storage-component.googleapis.com",
     "pubsub.googleapis.com",
-    "oslogin.googleapis.com",
+    "sql-component.googleapis.com",
+    "sqladmin.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
   ]
+}
+
+module "forseti-service-project" {
+  source          = "terraform-google-modules/project-factory/google//modules/shared_vpc"
+  version         = "~> 3.0"
+  org_id          = var.org_id
+  folder_id       = var.folder_id
+  billing_account = var.billing_account
+  name            = "ci-forseti-serv"
+  project_id      = "ci-forseti-serv-${var.project_suffix}"
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "serviceusage.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "pubsub.googleapis.com",
+    "sql-component.googleapis.com",
+    "sqladmin.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+  ]
+  shared_vpc = module.forseti-host-project.project_id
 }
 
 // Define a shared VPC network within the Forseti host project.
@@ -44,8 +90,9 @@ module "forseti-host-network-01" {
   source  = "terraform-google-modules/network/google"
   version = "1.1.0"
 
-  project_id      = "ci-forseti-service-${var.project_suffix}"
-  network_name    = "forseti-network"
+  project_id   = module.forseti-host-project.project_id
+  network_name = "forseti-network"
+
   shared_vpc_host = true
 
   secondary_ranges = {
@@ -88,26 +135,6 @@ resource "google_compute_router_nat" "forseti_host" {
   region  = google_compute_router.forseti_host.region
 }
 
-module "forseti-service-project" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 3.0"
-
-  org_id          = var.org_id
-  folder_id       = var.folder_id
-  billing_account = var.billing_account
-  name            = "ci-forseti-service"
-  project_id      = "ci-forseti-service-${var.project_suffix}"
-
-  activate_apis = [
-    "compute.googleapis.com",
-    "serviceusage.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
-    "iam.googleapis.com",
-  ]
-  shared_vpc         = "ci-forseti-host-project-${var.project_suffix}"
-  shared_vpc_subnets = ["projects/ci-forseti-host-project-${var.project_suffix}/regions/us-central1/subnetworks/forseti-subnetwork"]
-}
-
 module "forseti-service-network" {
   source  = "terraform-google-modules/network/google"
   version = "1.1.0"
@@ -127,7 +154,6 @@ module "forseti-service-network" {
     },
   ]
 }
-
 resource "google_compute_router" "forseti_service" {
   name    = "forseti-service"
   network = module.forseti-service-network.network_self_link
@@ -154,24 +180,3 @@ resource "google_compute_router_nat" "forseti_service" {
   project = module.forseti-service-project.project_id
   region  = google_compute_router.forseti_service.region
 }
-
-
-module "forseti-enforcer-project" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 3.0"
-
-  name = "ci-forseti-enforcer"
-
-  project_id      = "ci-forseti-enforcer-${var.project_suffix}"
-  org_id          = var.org_id
-  folder_id       = var.folder_id
-  billing_account = var.billing_account
-
-  activate_apis = [
-    "compute.googleapis.com",
-    "storage-api.googleapis.com",
-    "storage-component.googleapis.com",
-    "pubsub.googleapis.com",
-  ]
-}
-
